3a912d67cb3e388a59596000655176fa691cef21
Daniel Ksenofontov <daks@skbkontur.ru>
2022-03-05 06:43:31 +0500

    приборка

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-    dialogOpen();       
+    initYaMap();
+    initDialog();
     initPageNavigation();
     initMobileMenu();
     initInvertedMenu();
     initScrollAnimations();
 });
 
-let ok = false;                    
-window.addEventListener('scroll', function() {
-    if (ok === false) {
-        ok = true;    
-        setTimeout(() => {                    
-            let script = document.createElement('script');
-            script.src = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa33167d6a5ad01d3df9149c8df9021fc2f061bd0957b4fb627403944276120e2&amp;width=825&amp;height=520&amp;lang=ru_RU&amp;scroll=false';
-            document.getElementById('map').replaceWith(script);                        
-        }, 500)    
-    }
-});
+function initYaMap() {
+    let isMapLoaded = false;
 
-function dialogOpen() {
+    window.addEventListener('scroll', () => {
+        if (isMapLoaded) {
+            return;
+        }
+
+        isMapLoaded = true;
+        setTimeout(() => {
+            let script = document.createElement('script');
+            script.async = true;
+            script.src = 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aa33167d6a5ad01d3df9149c8df9021fc2f061bd0957b4fb627403944276120e2&amp;width=825&amp;height=520&amp;lang=ru_RU&amp;scroll=false';
+            document.getElementById('map').replaceWith(script);
+        }, 500)
+    });
+}
+
+function initDialog() {
     const dialogs = document.querySelectorAll('[data-dialog]');
+
     dialogs.forEach((element) => {
         dialogPolyfill.registerDialog(element);
     })
-    
+
     document.querySelectorAll('[data-dialog-open]').forEach((el) => {
-        el.addEventListener('click', () => {       
+        el.addEventListener('click', () => {
             const dialogId = el.getAttribute('data-dialog-open');
             const dialog = document.querySelector(`[data-dialog="${dialogId}"]`);
+
             dialog.showModal();
         });
     })
@@ -41,7 +50,7 @@ function initPageNavigation() {
 
     const navItems = document.querySelectorAll('[data-navigation-link]');
     const sections = document.querySelectorAll('[data-navigation-section]');
-    
+
     const observerOptions = {
         root: null,
         rootMargin: '0px 0px -400px',
@@ -60,24 +69,12 @@ function initPageNavigation() {
                 [].forEach.call(navItems, (item) => {
                     if (item === activeNavItem) {
                         item.classList.add('menu__link_active');
-                        // updateHistory(hash);
                         return;
                     }
                     item.classList.remove('menu__link_active');
                 });
             }
         });
-    }
-
-    function updateHistory(hash) {
-        if (window.location.hash === hash) {
-            return;
-        }
-        if (location.hash !== '') {
-            history.pushState({}, window.title, hash);
-            return;
-        }
-        history.replaceState({}, window.title, hash);
     }
 }
 
